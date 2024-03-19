f15c256ff1b2a81d72c8687ce01fa9575e37378b
mariaparreira <minesparreira99@gmail.com>
2024-03-19 11:54:40 +0000

    commit:
    - added a resources package with an application.conf file to hold the secret.
    
    - added authentication:
         * AppConfig: to load configuration values from an external configuration file into my app.
         * AuthMiddleware: sets up an authentication middleware to validate JWT tokens in http requests.
         * ToyRoutes: provides routes to generate JWT tokens for game IDs and handles authentication-related requests, ensuring that only authenticated users can access certain endpoints.
    
      - updated Main to use config and the authentication routes.

--- src/main/scala/minesweepergame/server/Command.scala
+++ src/main/scala/minesweepergame/server/Command.scala
@@ -1,9 +1,7 @@
 package minesweepergame.server
 
-import cats.effect.IO
 import io.circe.Codec
 import io.circe.generic.semiauto.deriveCodec
-import minesweepergame.game.GameSession
 
 final case class Command(row: Int, col: Int)
 

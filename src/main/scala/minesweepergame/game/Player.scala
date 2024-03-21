37553835cf9b324e105e50bfac31f90e35f0df11
mariaparreira <minesparreira99@gmail.com>
2024-03-21 11:53:46 +0000

    commit:
    - Added a final case class for Player with id and screenName fields.
    
    - Changed the route login/ to accept screenName and pass it along with the jwt token, which will decode jwt token to a Player.
    
    - Updated GameSession to accept a player type Player.
    
    - Also changed AuthMiddleware to extract the player information from claim. If the jwt is valid and contains a player, the player is authenticated.

--- src/main/scala/minesweepergame/game/Player.scala
+++ src/main/scala/minesweepergame/game/Player.scala
@@ -5,7 +5,7 @@ import io.circe.generic.semiauto._
 
 import java.util.UUID
 
-case class Player(id: UUID, screenName: String)
+final case class Player(id: UUID, screenName: String)
 
 object Player {
   implicit val playerCodec: Codec[Player] = deriveCodec
